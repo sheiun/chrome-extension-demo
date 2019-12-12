947e496b63b4673fccf3058efb13979465f3789a
SheiUn <me.sheiun@gmail.com>
2019-12-12 18:15:31 +0800

    feat: intercept requests and get the right answer

--- background.js
+++ background.js
@@ -11,3 +11,20 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     return true;
   }
 });
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    if (details.url.includes("friends")) {
+      fetch(details.url.replace("friends", "friend"))
+        .then(r => r.json())
+        .then(r => r.data.questions) // array
+        .then(r => r.map(q => q.choosenOption))
+        .then(r => {
+          chrome.storage.sync.set({ questions: r }, () => {
+            console.log("questions is set to " + r.toString());
+          });
+        });
+    }
+  },
+  { urls: ["*://*.buddymojo.com/api/v1/quiz/18*"] }
+);
--- popup.js
+++ popup.js
@@ -1,10 +1,18 @@
 let url =
   "https://cn.buddymojo.com/api/v1/quiz/18?type=friend&stats=1&userQuizId=1228288";
 
-chrome.runtime.sendMessage({ url, contentScriptQuery: "getAnswers" }, function(
-  response
-) {
+// chrome.runtime.sendMessage(
+//   { url, contentScriptQuery: "getAnswers" },
+//   response => {
+//     let div = document.createElement("div");
+//     div.innerText = response;
+//     document.querySelector("div").appendChild(div);
+//   }
+// );
+
+chrome.storage.sync.get(["questions"], function(result) {
   let div = document.createElement("div");
-  div.innerText = response;
+  div.innerText = result.questions;
   document.querySelector("div").appendChild(div);
+  console.log("Value currently is " + result.questions);
 });
