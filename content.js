fb12550760b063c80e706d71e446f74335027759
SheiUn <me.sheiun@gmail.com>
2019-12-12 18:49:11 +0800

    feat: add friend's name to storage

--- content.js
+++ content.js
@@ -1,7 +1,22 @@
-let title = document.querySelector(
-  "body > div.container > div.main-body.ng-scope > div.play.sync-page-form > div.Get-started > h2"
-).outerText;
+function loop() {
+  let splittedH2 = document
+    .querySelector(
+      "body > div.container > div.main-body.ng-scope > div.play.sync-page-form > div.Get-started > h2"
+    )
+    .outerText.split(" ");
+  if (splittedH2.length > 1) {
+    let friend = splittedH2[0];
+    alert(friend);
+    setFriend(friend);
+    return;
+  }
+  setTimeout(loop, 1000);
+}
 
-chrome.storage.sync.set({ key: value }, function() {
-  console.log("Value is set to " + value);
-});
+function setFriend(friend) {
+  chrome.storage.sync.set({ friend }, () => {
+    console.log("Title is set to " + friend);
+  });
+}
+
+loop();
